5ca87a2a9f164d3b986775456f13479ac4204214
venetucci <michelle.ann.harvey@gmail.com>
2016-01-24 12:45:31 -0800

    Refactored type coercion syntax for creating a Double

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -28,13 +28,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18, 0.2, 0.22]
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        let tipPercentages = [0.18, 0.2, 0.22]
+        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip: Double = 0
+        var total: Double = 0
         
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
+        if let billAmount = Double(billField.text!) {
+            tip = billAmount * tipPercentage
+            total = billAmount + tip
+        }
         
         tipLabel.text = String(format: "$%.2f", tip)
         totalLabel.text = String(format: "$%.2f", total)

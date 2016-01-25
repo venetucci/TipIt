ea35dffec40ffca0c8c5ec5d5d7b0338a09d89a7
venetucci <michelle.ann.harvey@gmail.com>
2016-01-24 17:16:42 -0800

    Create custom animations

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -8,18 +8,68 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet var containerView: UIView!
+    @IBOutlet weak var bottomContent: UIView!
+    @IBOutlet weak var billTitleStart: UILabel!
+    @IBOutlet weak var billTitleEnd: UILabel!
+    @IBOutlet weak var dollarLabel: UILabel!
+    
+    var firstTapHappened = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
+        billField.delegate = self
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        setInitialPosition()
+    }
+    
+    func setInitialPosition() {
+        containerView.transform = CGAffineTransformMakeTranslation(0, 150)
+        dollarLabel.transform = CGAffineTransformMakeTranslation(37, 0)
+        bottomContent.transform = CGAffineTransformMakeTranslation(0, 10)
+        billField.alpha = 0
+        bottomContent.alpha = 0
+        billTitleEnd.alpha = 0
+        billTitleStart.alpha = 1
+    }
+    
+    func animateToMainPosition() {
+        
+        UIView.animateWithDuration(0.37, animations: {
+            self.containerView.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.dollarLabel.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.billField.alpha = 1
+            }, completion: { (value: Bool) in
+            self.billField.becomeFirstResponder()
+        })
+        
+    }
+    
+    func showTipCalculation() {
+        
+        UIView.animateWithDuration(0.3, animations: {
+            self.bottomContent.transform = CGAffineTransformMakeTranslation(0, 0)
+            self.bottomContent.alpha = 1
+            self.billTitleEnd.alpha = 1
+            self.billTitleStart.alpha = 0
+        })
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        showTipCalculation()
+        return true
     }
 
     override func didReceiveMemoryWarning() {
@@ -45,6 +95,11 @@ class ViewController: UIViewController {
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
+        
+        if !firstTapHappened {
+            firstTapHappened = true
+            animateToMainPosition()
+        }
     }
     
     // Remove segmented controller borders
